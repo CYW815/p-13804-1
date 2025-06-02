c71fb1da767e4b1bfba62cd237be19087e622a36
CYW815 <cho0815y@gmail.com>
2025-06-02 10:33:07 +0900

    lab1-3

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -1,7 +1,23 @@
 package com.back;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    lab1();
+  }
+
+  private static void lab1() {
+    String input = """
+              등록
+              나의 죽음을 적들에게 알리지 말라.
+              """;
+    Scanner scanner = new Scanner(input);
+
+    String cmd = scanner.nextLine();
+    String content = scanner.nextLine();
+
+    System.out.println(cmd);
+    System.out.println(content);
   }
 }
